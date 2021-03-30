84c48c82871eea6b8f2c569c678899066140530e
Tezaen <Philippines420AD>
2021-03-29 17:20:05 -0700

    finished section 6 of Brad's React course

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Connecting DB
+connectDB();
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome to the Contact Keeper API' });
+})
+
+//Define routes tutorial https://www.youtube.com/watch?v=iM_S4RczozU
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const contacts = require('./routes/contacts');
+
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/contacts', contacts);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
