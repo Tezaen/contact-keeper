956ebbfe18d7e31691e687323f01346bc75bbf73
Tezaen <Philippines420AD>
2021-04-03 15:57:48 -0700

    did User stuff and made User model

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 //Connecting DB
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to the Contact Keeper API' });
 })
