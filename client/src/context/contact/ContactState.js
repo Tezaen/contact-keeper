5c5c1073b1fefb482f6dabc74f70edb558357869
Tezaen <Philippines420AD>
2021-06-07 17:40:59 -0700

    halfway thru section 8. fin lecture 55

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,78 @@
+import React, { useReducer } from 'react';
+import {v4 as uuid} from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Jill Johnson',
+                email: 'jill@gmail.com',
+                phone: '111-111-1111',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: 'Jack Dempsey',
+                email: 'jack_d@gmail.com',
+                phone: '111-222-1111',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Danny Johnson',
+                email: 'djohnson@gmail.com',
+                phone: '222-333-1111',
+                type: 'professional'
+            }
+        ]
+    };
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+    const addContact = contact => {
+        contact.id = uuid();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    }
+
+    // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    }
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    //Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    return (
+        <ContactContext.Provider
+            value={{
+                contacts: state.contacts,
+                addContact,
+                deleteContact
+            }}
+        >
+            { props.children }
+        </ContactContext.Provider>
+    );
+};
+
+export default ContactState;
