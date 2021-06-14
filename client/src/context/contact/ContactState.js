117a3b9d080f4d00c2752b3cee1f1366bae0c1ec
Tezaen <Philippines420AD>
2021-06-14 13:08:53 -0700

    finished sect 10, just need to deploy

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,25 +4,45 @@ import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
+    CLEAR_CONTACTS,
     CLEAR_FILTER,
     CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
     const initialState = {
-        contacts: [],
+        contacts: null,
         current: null, //Contact info displayed inside the contact form whenever you hit "update"
         filtered: null, //Array of contacts that will show up on search
         error: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Get Contacts
+    const getContacts = async () => {
+        // no need for config if ur not sending anything
+        try {
+            const res = await axios.get('/api/contacts'); // no need to send token as long as it is in localstorage
+    
+            dispatch({ 
+                type: GET_CONTACTS, 
+                payload: res.data 
+            });
+        } catch (error) {
+            dispatch({ 
+                type: CONTACT_ERROR,
+                payload: error.response.message
+            });
+        }
+    } 
 
     // Add Contact
     const addContact = async contact => {
@@ -48,8 +68,49 @@ const ContactState = props => {
     }
 
     // Delete Contact
-    const deleteContact = id => {
-        dispatch({ type: DELETE_CONTACT, payload: id });
+    const deleteContact = async id => {
+        try {
+            await axios.delete(`/api/contacts/${id}`); // no need to send token as long as it is in localstorage
+    
+            dispatch({
+                type: DELETE_CONTACT, 
+                payload: id 
+            });
+        } catch (error) {
+            dispatch({ 
+                type: CONTACT_ERROR,
+                payload: error.response.message
+            });
+        }
+    }
+
+    //Update Contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config); // no need to send token as long as it is in localstorage
+    
+            dispatch({ 
+                type: UPDATE_CONTACT, 
+                payload: res.data 
+            });
+        } catch (error) {
+            dispatch({ 
+                type: CONTACT_ERROR,
+                payload: error.response.message
+            });
+        }
+
+    }
+
+    // Clear Contacts
+    const clearContacts = () => { // When you log out, you want to clear the state of any contacts 
+        dispatch({ type: CLEAR_CONTACTS });
     }
 
     // Set Current Contact
@@ -60,11 +121,6 @@ const ContactState = props => {
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
-    }
-
-    //Update Contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact });
     }
 
     // Filter Contacts
@@ -84,13 +140,15 @@ const ContactState = props => {
                 current: state.current,
                 filtered: state.filtered,
                 error: state.error,
+                getContacts,
                 addContact,
                 deleteContact,
                 setCurrent,
                 clearCurrent,
                 updateContact,
                 filterContacts,
-                clearFilter
+                clearFilter,
+                clearContacts
             }}
         >
             { props.children }
