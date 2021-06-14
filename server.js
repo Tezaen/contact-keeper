59379306004c2251bb2377bb0e5d0ab95a0020ef
Tezaen <Philippines420AD>
2021-06-14 13:18:10 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,10 +12,6 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the Contact Keeper API' });
-})
-
 //Define routes tutorial https://www.youtube.com/watch?v=iM_S4RczozU
 const users = require('./routes/users');
 const auth = require('./routes/auth');
@@ -22,6 +20,14 @@ const contacts = require('./routes/contacts');
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/contacts', contacts);
+
+// Server statci assets in production
+if (process.env.NODE_ENV === ' production') {
+    // Set a static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
